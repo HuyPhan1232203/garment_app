0076b60ee152c5a50f15f01c332ac6876c6d0a78
Huy <Huyphan1232203@gmail.com>
2025-02-20 01:56:12 +0700

    Blank page

--- app/departmentPage/department.tsx
+++ app/departmentPage/department.tsx
@@ -1,35 +1,13 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React, { useEffect } from "react";
-import { defaultStyles } from "@/styles/default";
-import { Header } from "@/components/Header";
-import axios from "axios";
-import { useRouter } from "expo-router";
+import BlankPage from "@/components/BlankPage";
 
 const departmentPage = () => {
-  const router = useRouter();
-  const fetchDepartment = async () => {
-    try {
-      const res = await axios.get(
-        "https://api-xuongmay-dev.lighttail.com/api/department?pageIndex=1&pageSize=10"
-      );
-    } catch {
-      console.log("fetch error");
-    }
-  };
-  useEffect(() => {
-    fetchDepartment();
-  }, []);
   return (
-    <View style={defaultStyles.container}>
-      <View>
-        <Header text="Sản xuất - Danh sách tổ" />
-        <TouchableOpacity onPress={() => {
-            router.navigate("/taskPage/task");
-          }}>
-          <Text>Chuyển trang</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <BlankPage
+      api="https://api-xuongmay-dev.lighttail.com/api/department?pageIndex=1&pageSize=10"
+      headerTitle="Sản xuất - Danh sách tổ"
+    />
   );
 };
 
---
+++ components/BlankPage.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, View } from "react-native";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { defaultStyles } from "@/styles/default";
+import { Header } from "./Header";
+type pageProps = {
+  api: string;
+  headerTitle: string;
+};
+const BlankPage = ({ api, headerTitle }: pageProps) => {
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(api);
+    } catch {
+      console.log("fetch error");
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <View style={defaultStyles.container}>
+      <View>
+        <Header text={headerTitle} />
+      </View>
+    </View>
+  );
+};
+
+export default BlankPage;
+
+const styles = StyleSheet.create({});
